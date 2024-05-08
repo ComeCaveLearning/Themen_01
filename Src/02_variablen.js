a71907587a67d579a3913af93940ab734dffb305
U.K. <168725354+FreieKifferKomune@users.noreply.github.com>
2024-05-08 08:35:01 +0000

    Deklaration / werteinschätzung

---
+++ Src/02_variablen.js
@@ -0,0 +1,18 @@
+//console.log("hi");
+/**
+ * 
+ * 
+ * Block-Kommentar
+ * 
+ */
+/***** Variablen 01 ********/
+// ; befehl ende
+//console.log("hi");
+//console.log(hi);
+
+let firstName;  //Deklaration (Definition)
+console.log (firstName); // (ausgabe)
+firstName = "Max"; // wertzuweisung | assignment
+console.log (firstName); //ausgabe
+let familyName = "Mütze";
+console.log ("HI, ich bin " + firstName + " " + familyName + "!" );
