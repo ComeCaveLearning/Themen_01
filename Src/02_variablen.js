242e2d7212a0f9e77632b71a2b5778a2a6fe7870
U.K. <168725354+FreieKifferKomune@users.noreply.github.com>
2024-05-08 09:55:49 +0000

    03

--- Src/02_variablen.js
+++ Src/02_variablen.js
@@ -10,9 +10,44 @@
 //console.log("hi");
 //console.log(hi);
 
-let firstName;  //Deklaration (Definition)
-console.log (firstName); // (ausgabe)
-firstName = "Max"; // wertzuweisung | assignment
-console.log (firstName); //ausgabe
-let familyName = "Mütze";
-console.log ("HI, ich bin " + firstName + " " + familyName + "!" );
+// let firstName;  //Deklaration (Definition)
+// console.log (firstName); // (ausgabe)
+// firstName = "Max"; // wertzuweisung | assignment
+// console.log (firstName); //ausgabe
+// let familyName = "Mütze";
+// console.log ("HI, ich bin " + firstName + " " + familyName + "!" );
+
+/*
+
+Wertzuweisung 2 
+JS ist ein untypisierte Sprache!
+
+*/
+
+// let test;
+// console.log("inhalt: " + test); //ausgabe
+// console.log("Datentyp: " + typeof test); //ausgabe datentyp
+// console.log("---------------------");
+ 
+// test = "hi"; // 'hi' String
+// console.log("inhalt: " + test); //ausgabe
+// console.log("Datentyp: " + typeof test); //ausgabe datentyp
+// console.log("---------------------");
+
+// test = true ; // 'hi' String
+// console.log("inhalt: " + test); //ausgabe
+// console.log("Datentyp: " + typeof test); //ausgabe datentyp
+// console.log("---------------------");
+
+/* Variablen und konstanten in js*/
+
+// variable
+let test; //variable deklaration
+test = "hi" // wertzuweisung
+test = "hello" // wert überschreiben
+console.log("inhalt:" + test); // ausgabe
+
+
+//konstante 
+const test2 = "1";
+console.log("inhalt: " + test2);// ausgabe
