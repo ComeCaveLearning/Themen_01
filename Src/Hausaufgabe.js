c6703ce5ea5ddb44357bd9fd2a957289bdd8558e
U.K. <168725354+FreieKifferKomune@users.noreply.github.com>
2024-05-09 14:40:33 +0000

    hausaufgabe

--- Src/Hausaufgabe.js
+++ Src/Hausaufgabe.js
@@ -12,4 +12,23 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 const prompt = require('prompt-sync')({sigint: true});
 
 
-console.log("2" + "2" );
+/*********   CODE CHALLENGE 01 **********/
+ 
+/*
+Geben Sie über eine Prompt-Anforderung eine Zahl ein.
+Geben Sie über eine Prompt-Anforderung eine zweite Zahl ein.
+Berechnen Sie die Summe der beiden Zahlen.
+Geben Sie die Summe der Zahlen in die Konsole aus:
+"Die Summe der Zahlen ist: summe"
+*/
+
+const prompt = require('prompt-sync')({sigint: true});
+
+let sunnand =  parseFloat(prompt("Zahl?: "));
+let second =  parseFloat(prompt("Zahl?: "));
+let summe = sunnand + second;
+console.log("Die Summe der Zahlen ist", summe);
+In JavaScript werden Benutzereingaben mit prompt standardmäßig als Zeichenfolgen zurückgegeben. 
+Wenn Sie also zwei Zahlen eingeben, werden sie als Zeichenfolgen interpretiert, nicht als Zahlen. 
+Wenn Sie zwei Zeichenfolgen mit dem +-Operator verbinden, führt dies zur Konkatenierung der Zeichenfolgen, nicht zur Addition der Zahlen.
+Um die Zeichenfolgen in Zahlen umzuwandeln, können Sie die Funktion parseInt oder parseFloat verwenden. 
